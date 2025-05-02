03d24442397f6ebfd4dd95aaf5dc2d71d8ba2bdb
Alex Kappler <agtkappler@gmail.com>
2025-05-01 18:13:54 -0600

    Temp class for repository

--- java-recipes/src/main/java/com/repositories/TempClass.java
+++ java-recipes/src/main/java/com/repositories/TempClass.java
@@ -1,17 +1,18 @@
-package com.recipes;
+package com.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import com.recipes.Recipe;
 
 import java.util.List;
+public class TempClass {}
 
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-
-//    @Query("select * from lu_recipes;")
-//    List<Recipe> findRecipes();
-    List<Recipe> findAll();
-
-//    @Query("select * from lu_recipes where rowid = ?1")
-    Recipe findById(long id);
-}
+//public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+//
+////    @Query("select * from lu_recipes;")
+////    List<Recipe> findRecipes();
+//    List<Recipe> findAll();
+//
+////    @Query("select * from lu_recipes where rowid = ?1")
+//    Recipe findById(long id);
+//}
