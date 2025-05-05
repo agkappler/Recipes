139dd43418dc1617a4aafa9d80082a6b3aac346b
Alex Kappler <agtkappler@gmail.com>
2025-05-05 16:10:07 -0600

    Add photo and formatting to about

--- recipe-site/app/about/page.tsx
+++ recipe-site/app/about/page.tsx
@@ -1,7 +1,9 @@
-import { Box, Typography } from "@mui/material"
+import { Box, Typography } from "@mui/material";
+import Image from "next/image";
 
 export default function About() {
-    return <Box className="container mx-auto px-4 py-8">
-        <Typography variant="h4" component="h1" gutterBottom>Alex Kappler</Typography>
+    return <Box className="flex flex-col justify-center align-items-center container mx-auto px-4 py-8">
+        <Image src={`/Alex_Kappler_Picture.jpg`} alt={"Alex Kappler"} width="128" height="128" className="m-auto" />
+        <Typography variant="h4" component="h1" textAlign="center">Alex Kappler</Typography>
     </Box>
 }
