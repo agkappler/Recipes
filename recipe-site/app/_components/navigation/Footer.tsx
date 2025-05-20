75f53b89ff87657f14ba5ed26b275855daf5674f
Alex Kappler <agtkappler@gmail.com>
2025-05-20 14:35:53 -0600

    Fix lint error

--- recipe-site/app/_components/navigation/Footer.tsx
+++ recipe-site/app/_components/navigation/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer: React.FC = () => {
         <footer className="bg-gray-100 mt-10">
             <Box className="container" color="primary" width="100">
                 <Typography variant="body1" color="textSecondary" textAlign="center">
-                    This site is maintained by Alex "Fargo" Kappler.
+                    {"This site is maintained by Alex 'Fargo' Kappler."}
                 </Typography>
             </Box>
         </footer>
