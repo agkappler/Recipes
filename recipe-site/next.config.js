e6f790b16bac9e98b4361a516d5d4ef02532171b
Alex Kappler <agtkappler@gmail.com>
2025-05-20 18:24:25 -0600

    Simplify things

--- recipe-site/next.config.js
+++ recipe-site/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { webpack : { devtool: false } }
+const nextConfig = {
+    reactStrictMode: true,
+    output: 'standalone',
+    experimental: {
+        serverActions: true, // only if using App Router
+    },
+    // webpack: { devtool: false }
+};
 
 module.exports = nextConfig
