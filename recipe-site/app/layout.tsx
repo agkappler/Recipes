6919153d05c2df6726df94a5418515a6d18b0f10
Alex Kappler <agtkappler@gmail.com>
2025-05-20 18:48:57 -0600

    Simplify root layout again

--- recipe-site/app/layout.tsx
+++ recipe-site/app/layout.tsx
@@ -1,24 +1,25 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+// import type { Metadata } from 'next'
+// import { Inter } from 'next/font/google'
 // import { Footer } from './_components/navigation/Footer'
 // import { Navbar } from './_components/navigation/Navbar'
 // import { ThemeRegistry } from './_components/ThemeRegistry'
 import './globals.scss'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-    title: 'Fargopolis',
-    description: 'Development by Fargo',
-    icons: {
-        icon: '/icon.png'
-    }
-}
+// export const metadata: Metadata = {
+//     title: 'Fargopolis',
+//     description: 'Development by Fargo',
+//     icons: {
+//         icon: '/icon.png'
+//     }
+// }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={`flex flex-col min-h-screen ${inter.className}`}>
+            <body className={`flex flex-col min-h-screen`}>
+                {/* <body className={`flex flex-col min-h-screen ${inter.className}`}> */}
                 {/* <ThemeRegistry>
                     <Navbar /> */}
                 <main className="flex-grow">{children}</main>
